ec7aa723d218d9177296c948e6aeb0da10ac494e
Zack ! <126048807+Lz-Zack@users.noreply.github.com>
2023-12-28 21:43:39 +0530

    Create ping.js

---
+++ plugins/ping.js
@@ -0,0 +1,17 @@
+const { Alexa, isPrivate} = require("../lib/");
+Alexa(
+  {
+    pattern: "ping",
+    fromMe: isPrivate,
+    desc: "To check ping",
+    type: "user",
+  },
+  async (message, match) => {
+    const start = new Date().getTime();
+    await message.sendMessage(message.jid,"```Ping!```");
+    const end = new Date().getTime();
+    return await message.sendMessage(message.jid,
+      "*Pong!*\n ```" + (end - start) + "``` *ms*"
+    );
+  }
+);
