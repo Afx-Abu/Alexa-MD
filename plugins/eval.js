4ac99d418d81839415c77d63b2bf27431abe864d
Zack ! <126048807+Lz-Zack@users.noreply.github.com>
2023-12-29 15:10:01 +0530

    Create eval.js

---
+++ plugins/eval.js
@@ -0,0 +1,20 @@
+const {
+  Alexa,
+} = require("../lib/");
+const util = require("util");
+const config = require("../config");
+
+
+
+Alexa({pattern:'eval', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
+  if (match.startsWith(">")) {
+    //const m = message;
+    try {
+      let evaled = await eval(`${match.replace(">", "")}`);
+      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+      await message.reply(evaled);
+    } catch (err) {
+      await message.reply(util.format(err));
+    }
+  }
+});
