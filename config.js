d81d79f0ba5f3c7cba0322b84d426e1aaf134409
Zack ! <126048807+Lz-Zack@users.noreply.github.com>
2024-01-04 15:59:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,13 @@
-const { Sequelize } = require("sequelize");
-const fs = require("fs");
-
-if (fs.existsSync("config.env")) {
-  require("dotenv").config({ path: "./config.env" });
-}
-
-const toBool = (x) => x === "true";
-
+const { Sequelize } = require("sequelize");const fs = require("fs");if (fs.existsSync("config.env")) {require("dotenv").config({ path: "./config.env" });}const toBool = (x) => x === "true";
 const DATABASE_URL = process.env.DATABASE_URL || "./lib/database.db";
 
 module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID: process.env.SESSION_ID || "idvu3oj6_XASENA_w46_XASENA_nb8c",
+  SESSION: process.env.SESSION.trim() || '',
   LANG: process.env.LANG || "EN",
-  HANDLERS:
-    process.env.HANDLER === "false" || process.env.HANDLER === "null"
-      ? "^"
-      : "^",
+  HANDLERS:process.env.HANDLER === "false" || process.env.HANDLER === "null"? "^": "^",
   STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'ᴊᴀʀᴠɪꜱ;ᴡᴀʙᴏᴛ',
   AUDIO_DATA: process.env.AUDIO_DATA || 'ʟᴏᴋɪ-xᴇʀ;ᴊᴀʀᴠɪꜱ;https://i.imgur.com/ep3UbBP.jpg',
   RMBG_KEY: process.env.RMBG_KEY || false,
@@ -35,21 +24,5 @@ module.exports = {
   BOT_NAME: process.env.BOT_NAME || "Alexa",
   WORK_TYPE: process.env.WORK_TYPE || "public",
   DATABASE_URL: DATABASE_URL,
-  DATABASE:
-    DATABASE_URL === "./lib/database.db"
-      ? new Sequelize({
-          dialect: "sqlite",
-          storage: DATABASE_URL,
-          logging: false,
-        })
-      : new Sequelize(DATABASE_URL, {
-          dialect: "postgres",
-          ssl: true,
-          protocol: "postgres",
-          dialectOptions: {
-            native: true,
-            ssl: { require: true, rejectUnauthorized: false },
-          },
-          logging: false,
-        }),
+  DATABASE:DATABASE_URL === "./lib/database.db"? new Sequelize({dialect: "sqlite",storage: DATABASE_URL,logging: false,}): new Sequelize(DATABASE_URL, {dialect: "postgres",ssl: true,protocol: "postgres",dialectOptions: {native: true,ssl: { require: true, rejectUnauthorized: false },},logging: false,}),
 };
