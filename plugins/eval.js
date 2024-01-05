7ebbed30156bd6889dfcd6dd1811dba1aa46491f
Zack ! <126048807+Lz-Zack@users.noreply.github.com>
2024-01-05 14:20:03 +0530

    Update eval.js

--- plugins/eval.js
+++ plugins/eval.js
@@ -1,14 +1,9 @@
-const {
-  Alexa,
-} = require("../lib/");
+const { Alexa } = require("../lib/");
 const util = require("util");
 const config = require("../config");
 
-
-
 Alexa({pattern:'eval', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
   if (match.startsWith(">")) {
-    //const m = message;
     try {
       let evaled = await eval(`${match.replace(">", "")}`);
       if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
